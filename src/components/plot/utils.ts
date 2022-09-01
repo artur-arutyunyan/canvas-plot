1a391f8b5fb7f5e57dc51a7b6766275132ac686f
Artur Arutyunyan <tom.soier@mail.ru>
2022-09-01 22:33:53 +0300

    styleless commit

--- src/components/plot/utils.ts
+++ src/components/plot/utils.ts
@@ -1,61 +1,51 @@
 import {Point} from "./index";
 
 export const drawPlot = (ctx: CanvasRenderingContext2D, plotData: Point[]) => {
-    const WIDTH = ctx.canvas.width;
-    const HEIGHT = ctx.canvas.height;
 
-    console.log(plotData)
-    const MAX_Y = Math.max(...plotData.map(({x, y}) => y)) + 100;
+    const WIDTH = ctx.canvas.width - 100;
+    const HEIGHT = ctx.canvas.height - 50;
+    const MAX_Y = Math.max(...plotData.map(({x, y}) => y))<100?100:Math.max(...plotData.map(({x, y}) => y))+50;
     const MAX_X = Math.max(...plotData.map(({x, y}) => x));
-
-
-    const MIN_Y = Math.min(...plotData.map(({x, y}) => y))
+    const MIN_Y = Math.min(...plotData.map(({x, y}) => y))<100?0:Math.min(...plotData.map(({x, y}) => y));
     const MIN_X = Math.min(...plotData.map(({x, y}) => x));
-    console.log(MAX_Y, MIN_Y)
-
-    function drawCircle( x: any, y: any, radius: any, fill: any, stroke: any, strokeWidth: any) {
-        ctx.beginPath()
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
-        if (fill) {
-            ctx.fillStyle = fill
-            ctx.fill()
-        }
-        if (stroke) {
-            ctx.lineWidth = strokeWidth
-            ctx.strokeStyle = stroke
-            ctx.stroke()
-        }
-    }
 
 
-    // ctx.rotate(-Math.PI/2);
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    drawCircle(0,0, 50, 'blue', undefined, undefined);
-    ctx.fillStyle = 'green'
-    ctx.fillRect(-10, -10, 20, 50);
-
-
-    ctx.transform(1, 0, 0, -1, 0, HEIGHT);
-    // ctx.transform(WIDTH/(MAX_X-MIN_X), 0, 0, -1, MIN_X, HEIGHT);
-    // ctx.translate(0, -HEIGHT)
-
-    drawCircle(0,0, 50, 'blue', undefined, undefined);
-
-    // ctx.lineWidth = 15;
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    plotData.forEach(({x,y}) => {
-        ctx.lineTo((x-MIN_X)*WIDTH/(MAX_X-MIN_X),(y-MIN_Y)*HEIGHT/(MAX_Y-MIN_Y))
-    })
+    // координатные оси
+    ctx.moveTo(50,10);
+    ctx.lineTo(50,HEIGHT);
+    ctx.lineTo(WIDTH+80,HEIGHT);
+    ctx.strokeStyle = "pink";
     ctx.lineWidth = 3;
     ctx.stroke();
-    //
-    // ctx.font = '250px serif';
-    // ctx.fillStyle = "green";
-    // plotData.forEach(({x, y}) => {
-    //     ctx.fillText(y.toString(), x, 100);
-    // })
+
+    // arrows
+    ctx.moveTo(40,30);
+    ctx.lineTo(50,10);
+    ctx.lineTo(60,30);
+    ctx.stroke();
+    ctx.moveTo(WIDTH+60,HEIGHT+10);
+    ctx.lineTo(WIDTH+80,HEIGHT);
+    ctx.lineTo(WIDTH+60,HEIGHT-10);
+    ctx.stroke();
+
+
+
+    ctx.beginPath();
+    ctx.moveTo(50, HEIGHT-(plotData[0].y-MIN_Y)*HEIGHT/(MAX_Y-MIN_Y));
+    ctx.strokeStyle  = "green";
+    ctx.font = '20px serif';
+    ctx.fillStyle = "green";
+    plotData.forEach(({x,y}, index) => {
+
+        ctx.lineTo((x-MIN_X)*WIDTH/(MAX_X-MIN_X)+50,HEIGHT-(y-MIN_Y)*HEIGHT/(MAX_Y-MIN_Y));
+        ctx.fillText(y.toString(), (x-MIN_X)*WIDTH/(MAX_X-MIN_X)+30, HEIGHT-(y-MIN_Y)*HEIGHT/(MAX_Y-MIN_Y)-20);
+        ctx.fillText(x.toString(), (x-MIN_X)*WIDTH/(MAX_X-MIN_X)+30, HEIGHT+30);
+    })
+    ctx.lineWidth = 5;
+    ctx.stroke();
 
 
 }
